e4913664a660c7700273a7a3661c288e94f8a395
Charles23R <renaudcharles14@hotmail.com>
2018-03-14 17:04:19 -0400

    All done Liste dynamique v.5 Bug fixed

---
+++ Liste/src/Entreprise.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Created by RenCh1732786 on 2018-01-22.
+ */
+public class Entreprise {
+
+    private String nom="";
+    private Adresse adresse=null;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public Entreprise ajouterEntreprise(){
+        Scanner sc=new Scanner(System.in);
+        Entreprise nouvelleEntreprise=new Entreprise();
+
+        System.out.println("Entreprise : ");
+        System.out.println("Nom :");
+        nouvelleEntreprise.setNom(sc.next());
+
+        nouvelleEntreprise.setAdresse(new Adresse());
+        nouvelleEntreprise.setAdresse(getAdresse().ajouterAdresse());
+
+        return nouvelleEntreprise;
+    }
+
+    public void afficherEntreprise(){
+        System.out.println("Entreprise : ");
+        System.out.println("Nom : "+nom);
+
+        adresse.afficherAdresse();
+    }
+}
---
+++ Liste/src/Telephone.java
@@ -0,0 +1,27 @@
+/**
+ * Created by RenCh1732786 on 2018-01-22.
+ */
+public class Telephone {
+
+    private String info=" ";
+    private String numero=" ";
+
+    //Getters
+    public String getInfo() {
+        return info;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+
+    //Setters
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
